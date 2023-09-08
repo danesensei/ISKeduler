06e4a07a5cc3078f776bbe18259a3e359941048b
Daniel Timbal <danesensei@yahoo.com>
2023-09-08 23:06:57 +0800

    Create RegistrationActivity.java

---
+++ RegistrationActivity.java
@@ -0,0 +1,94 @@
+package com.example.iskeduler;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.AuthResult;
+
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText inputName, inputEmail, inputPassword, inputUsername;
+    private Button btnRegister;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth auth;
+    private DatabaseReference usersRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance();
+
+        // Initialize Firebase Realtime Database reference for users
+        usersRef = FirebaseDatabase.getInstance().getReference("users");
+
+        inputName = findViewById(R.id.input_name);
+        inputEmail = findViewById(R.id.input_email);
+        inputPassword = findViewById(R.id.input_password);
+        inputUsername = findViewById(R.id.input_username);
+        btnRegister = findViewById(R.id.btn_register);
+        progressBar = findViewById(R.id.progressBar);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String name = inputName.getText().toString().trim();
+                final String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+                final String username = inputUsername.getText().toString().trim();
+
+                if (TextUtils.isEmpty(name) || TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty(username)) {
+                    Toast.makeText(getApplicationContext(), "All fields are required", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                // Create a new user with email and password
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = auth.getCurrentUser();
+
+                                    // Save user data to Firebase Realtime Database
+                                    User newUser = new User(name, email, username);
+                                    usersRef.child(user.getUid()).setValue(newUser);
+
+                                    Toast.makeText(getApplicationContext(), "Registration successful!", Toast.LENGTH_SHORT).show();
+                                    progressBar.setVisibility(View.GONE);
+                                    finish();
+                                } else {
+
+                                    Exception exception = task.getException();
+                                    if (exception != null) {
+                                        Toast.makeText(getApplicationContext(), "Registration failed: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                    progressBar.setVisibility(View.GONE);
+                                }
+                            }
+                        });
+
+            }
+        });
+    }
+}
